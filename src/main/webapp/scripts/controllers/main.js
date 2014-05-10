c1c77e91a24fe957a65a01977809142f618f703c
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2014-05-10 12:41:51 +0200

    Added AngularJS as frontend, along with Grunt, Bower and Karma for building and testing

---
+++ src/main/webapp/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('broadcastStudioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
