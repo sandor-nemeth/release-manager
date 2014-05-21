a7406460cd30934c31fdec5d1f190ae931907c10
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2014-05-21 06:12:07 +0200

    Added configurations to make use of every added library in the project.

--- backend/src/main/java/releasemanager/Application.java
+++ backend/src/main/java/releasemanager/Application.java
@@ -1,19 +1,91 @@
 package releasemanager;
 
+import liquibase.integration.spring.SpringLiquibase;
+import lombok.extern.slf4j.Slf4j;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.util.Assert;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.metamodel.EntityType;
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Set;
 
 /**
  * Created by sandornemeth on 20/05/14.
  */
 @Configuration
 @EnableAutoConfiguration
+@EnableJpaRepositories
+@ComponentScan
+@Slf4j
 public class Application extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired private DataSource dataSource;
+
+    private net.sf.ehcache.CacheManager cacheManager;
+
+
+    @Bean
+    public Mapper mapper() throws Exception {
+        DozerBeanMapper mapper = new DozerBeanMapper();
+        mapper.setMappingFiles(Arrays.asList("dozer-mappings.xml"));
+        return mapper;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        log.debug("Starting Ehcache");
+        cacheManager = net.sf.ehcache.CacheManager.create();
+        log.debug("Registring Ehcache Metrics gauges");
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        for (EntityType<?> entity : entities) {
+
+            String name = entity.getName();
+            if ( name == null ) {
+                name = entity.getJavaType().getName();
+            }
+            Assert.notNull(name, "entity cannot exist without a identifier");
+
+            net.sf.ehcache.Cache cache = cacheManager.getCache(name);
+            if (cache != null) {
+                cache.getCacheConfiguration().setTimeToLiveSeconds(env.getProperty("cache.timeToLiveSeconds", Integer.class, 3600));
+            }
+        }
+        EhCacheCacheManager ehCacheManager = new EhCacheCacheManager();
+        ehCacheManager.setCacheManager(cacheManager);
+        return ehCacheManager;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/db/changelog/db.changelog-master.yaml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
     }
 }
